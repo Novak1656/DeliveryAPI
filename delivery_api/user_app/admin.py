f3af8b775dedc8fea0d622a59b80868fe70f3869
acclrtrrr <andrey.novikov2001@yandex.ru>
2023-02-03 20:13:55 +0300

    Added user model in admin panel

--- delivery_api/user_app/admin.py
+++ delivery_api/user_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'username', 'email', 'role', 'is_active', 'is_superuser', 'created_at')
+    list_display_links = ('pk', 'username')
+    search_fields = ('username', 'first_name', 'last_name')
+    list_filter = ('role', 'is_superuser', 'is_active')
+    save_as = True
